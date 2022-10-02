3c7f384220c8a622dbcb7e1b3ad1f6c5bc540379
Gonzalo Fernandez Yaique <fygonzalo@gmail.com>
2022-10-01 21:55:41 -0300

    fix spawnplayer serialization

--- src/game/messages/responses/spawnplayer.h
+++ src/game/messages/responses/spawnplayer.h
@@ -1,37 +1,38 @@
-
 #ifndef OPENAO_GAME_MESSAGES_RESPONSES_SPAWNPLAYER_H
 #define OPENAO_GAME_MESSAGES_RESPONSES_SPAWNPLAYER_H
 
+#include <cstdint>
+
 #include "model/character.h"
 #include "serialization/attributes.h"
-#include <cstdint>
+#include "serialization/equipment.h"
+
 namespace Game::Messages::Responses {
 
 struct SpawnPlayer {
   static const uint16_t type = 0x01;
 
-  uint32_t entityid;
+  uint32_t entityid = 1007289969;
 
   Model::Character c;
 
   template <typename Archive>
   void serialize(Archive& archive) {
     archive.write(entityid);
-    archive.set(0, 4);
+    archive.write(c.position.orientation);
     archive.write(c.position.x);
     archive.write(c.position.y);
     archive.write(c.name);
     archive.write(c.title);
-    archive.set(0, 5);
-    archive.write(c.position.orientation);
-    archive.write(c.shape);
+    archive.set(0, 9);
     Serialization::serialize(archive, c.attributes);
     archive.write(c.faction);
-    archive.set(0, 1);
-    archive.set(0, 2);
+    archive.set(0, 1); // ?
+    archive.set(0, 2); // Team ?
     Serialization::serialize(archive, c.equipment);
     archive.set(0, 13);
-    archive.set(0, 13);
+    archive.set(0, 13); // League ?
+    archive.set(0, 6);
   }
 
 };
