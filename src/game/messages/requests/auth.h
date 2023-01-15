82d373c987df831f49a8c4c4b5d9fe796a00fece
Gonzalo Fernandez Yaique <fygonzalo@gmail.com>
2023-01-14 23:49:19 -0300

    refactor: game auth request sends version
    The first field on game auth request represents the version of the game.
    It's the only field that changed between to captures, one which returned
    "Wrong version" and the other allowing to connect succesfully.
    
    May be hashed/encoded.

--- src/game/messages/requests/auth.h
+++ src/game/messages/requests/auth.h
@@ -9,7 +9,7 @@ namespace Game::Messages::Requests {
 struct Auth {
   static const uint16_t type = 0x02;
 
-  std::array<uint8_t, 17> interface;
+  std::array<uint8_t, 17> version;
   uint32_t account_id;
   uint32_t character_id;
   uint32_t stage;
@@ -17,7 +17,7 @@ struct Auth {
 
   template <typename Archive>
   void deserialize(Archive& archive) {
-    archive.read(interface);
+    archive.read(version);
     archive.read(account_id);
     archive.read(character_id);
     archive.read(stage);
