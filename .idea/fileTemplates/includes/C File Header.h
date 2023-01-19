0b4c8d05eea5f3c946535ef20473a5608d290e8c
Gonzalo Fernandez Yaique <fygonzalo@gmail.com>
2023-01-19 10:58:52 -0300

    chore: delete clion files
    While I think it's useful to share the project configurations, it may
    lead to the commit of unwanted files/data.
    
    On top of that, pre-commit already takes care of formatting and is IDE
    agnostic, so it's not that neccesary.

--- .idea/fileTemplates/includes/C File Header.h
+++
@@ -1,7 +0,0 @@
-#set ($NAMESPACE = $DIR_PATH.replace("/", "::"))
-#if ($NAMESPACE.startsWith("src::"))
-    #set ($NAMESPACE = $NAMESPACE.replaceFirst("src::", ""))
-#end
-#set ($NAMESPACE = ${PROJECT_NAME} + "::" + ${NAMESPACE})
-#set ($GUARD = $NAMESPACE.replace("::","_") + "_" + $FILE_NAME.replace(".", "_"))
-#set ($GUARD = $GUARD.toUpperCase())
--- .idea/fileTemplates/internal/C Header File.h
+++
@@ -1,9 +0,0 @@
-#parse("C File Header.h")
-#[[#ifndef]]# ${GUARD}
-#[[#define]]# ${GUARD}
-
-namespace ${NAMESPACE} {
-
-}
-
-#[[#endif]]# //${GUARD}
